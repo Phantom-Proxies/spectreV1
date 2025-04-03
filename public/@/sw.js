1eea36dd75eac8bab61e7cdb08d89664280c1473
Kitten Tech <104227728+DarkCat736@users.noreply.github.com>
2025-04-02 20:39:11 -0500

    Delete public directory

--- public/@/sw.js
+++
@@ -1,9 +0,0 @@
-// ALL CREDITS GO TO ULTRAVIOLET (by Titanium Network) FOR THIS SCRIPT
-
-importScripts('/@/uv.bundle.js');
-importScripts('/@/uv.config.js');
-importScripts(__uv$config.sw || '/@/uv.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
